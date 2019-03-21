b0c9961c161547fa9b6bf0e5472b185ee3ea789a
taohua <liuy1994g@gmail.com>
2019-03-21 16:54:21 +0800

    CartList

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // import './List.dart';
 //import './Widgets.dart';
-import './TutorialHome.dart';
+//import './TutorialHome.dart';
+import './Product.dart';
 
 void main() => runApp(new MyApp());
